efa29030dcf2e7f60c3dc5c76da4798c12ec8e34
Federico Deniard <fededeniard@gmail.com>
2024-05-09 14:05:35 -0300

    ordenado los archivos de algoritmos de ordenamiento

--- practicas/ordenamiento/insertion_sort.py
+++ practicas/ordenamiento/insertion_sort.py
@@ -1,12 +1,5 @@
-import time
-
-numbers = [3, 6, 2, 8, 4, 6, 1, 9, 5]
-
 def insertion_sort(array):
     for i in range(1,len(array)):
         for j in range(i,0,-1):
             if array[j] < array[j-1]:
                 array[j],array[j-1] = array[j-1],array[j]
-
-# insertion_sort(numbers)
-# print(numbers)
