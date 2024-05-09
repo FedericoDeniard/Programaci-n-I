ae3e2555d3bea0d40d09f33458973cfc684c55cd
Federico Deniard <fededeniard@gmail.com>
2024-05-09 15:46:22 -0300

    binary search y comparación con algoritmo de búsqueda básico

--- practicas/ordenamiento/busqueda/binary_search.py
+++ practicas/ordenamiento/busqueda/binary_search.py
@@ -1,5 +1,3 @@
-numbers = [2, 4, 7, 9, 12, 14, 17, 19, 22, 25, 28, 30, 33, 36, 39, 42, 45, 47, 50, 53]
-
 def binary_search(array: list, value: int, start, end) -> int:
     index = None
     if start > end:
@@ -14,6 +12,3 @@ def binary_search(array: list, value: int, start, end) -> int:
         return binary_search(array, value, start, average-1)
     elif array[average] < value:
         return binary_search(array, value, average+1,end)
-
-print(binary_search(numbers, 13,0,len(numbers)-1))
-
